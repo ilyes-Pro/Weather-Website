a76f2e788aa1d8dd7c00090e5072178227684a25
ilyes <ilyesprogrammer42@gmail.com>
2025-06-26 20:36:07 +0100

    APi and cleanup

--- src/components/cardWather.js
+++ src/components/cardWather.js
@@ -8,21 +8,91 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Divider from '@mui/material/Divider';
 import Grid from '@mui/material/Grid';
-
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 // icon
 import WbSunnyIcon from '@mui/icons-material/WbSunny';
 import CloudIcon from '@mui/icons-material/Cloud';
 
-const bull = (
-    <Box
-        component="span"
-        sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-    >
 
-    </Box>
-);
-
+let cancelAxios = null;
 export default function CardWather() {
+
+
+
+    const [todayDate, setTodayDate] = useState("");
+
+    const [weatherData, setweatherData] = useState({
+        temperature: 0,
+        Tmax: 0,
+        Tmin: 0
+    });
+
+    useEffect(() => {
+
+        console.log("rdnerign the componenting")
+        const today = new Date().toISOString().split("T")[0];
+
+
+        console.log(today);
+        setTodayDate(today);
+
+        axios.get('https://api.open-meteo.com/v1/forecast?latitude=34.858891&longitude=-1.729586&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m',
+            {
+
+                cancelToken: new axios.CancelToken((e) => {
+                    cancelAxios = e
+                })
+            }
+        )
+            .then(response => {
+                const allTemps = response.data.hourly.temperature_2m;
+                const allTimes = response.data.hourly.time;
+
+                // هده خاطأة 
+                //             const tempA = {
+
+                //                 Tday: response.data.hourly.time,
+                //                 Tf: response.data.hourly.temperature_2m
+                //             }
+                //                 ;
+                //             const todayTemps = tempA.filter(temp => temp.Tday.startsWith(todayDate));
+                //             const Temp = todayTemps.map(temp => temp.Tf);
+                //             const maxTemp = Math.max(...Temp);
+                //             const minTemp = Math.min(...Temp);
+
+                //             setweatherData({
+                //                 temperature: response.data.current.temperature_2m,
+                //                 Tmax: maxTemp,
+                //                 Tmin: minTemp
+                //             });
+                console.log("ok")
+                const tempsToday = allTimes
+                    .map((time, index) => ({ time, temp: allTemps[index] }))
+                    .filter(entry => entry.time.startsWith(today));
+
+                const tempsOnly = tempsToday.map(entry => entry.temp);
+                const maxTemp = Math.max(...tempsOnly);
+                const minTemp = Math.min(...tempsOnly);
+                console.log(response.data.current.temperature_2m)
+                setweatherData({
+                    temperature: response.data.current.temperature_2m,
+                    Tmax: maxTemp,
+                    Tmin: minTemp
+                });
+            })
+            .catch(error => {
+                console.error('Error fetching weather data:', error);
+            });
+
+        return () => {
+            console.log("cansul")
+            cancelAxios();
+        }
+
+    }, []);
+
+
     return (
         <>
 
@@ -46,11 +116,11 @@ export default function CardWather() {
                             <Grid size={{ xs: 6 }}>
                                 <div style={{ display: 'flex', alignItems: 'center', gap: 15, }}>
 
-                                    <Typography color="main" variant="h1">38</Typography>
+                                    <Typography color="main" variant="h1">{weatherData.temperature}</Typography>
                                     < WbSunnyIcon sx={{ color: "yellow", fontSize: 70, }} />
                                 </div>
                                 <Typography color="main" mb={4} variant="h6">broken clouds</Typography>
-                                <Typography color="main" sx={{ fontSize: 14, fontWeight: 200, }}>الصغرى:38 / الكبرى:40</Typography>
+                                <Typography color="main" sx={{ fontSize: 14, fontWeight: 200, }}>الصغرى: {weatherData.Tmin} / الكبرى: {weatherData.Tmax}</Typography>
                             </Grid>
                             <Grid size={{ xs: 6 }}>
                                 <CloudIcon sx={{ color: "white", fontSize: 200, }} />
